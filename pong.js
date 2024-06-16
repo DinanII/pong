0ea35b86bbf8cf5aa916e414606ead12aff376e8
Dinan <dinanvanw@gmail.com>
2024-06-16 22:44:38 +0200

    Initial Commit
    Completed the the tutorial

---
+++ pong.js
@@ -0,0 +1,174 @@
+// Initialization
+// Variables
+let board;
+let boardWidth = 500;
+let boardHeight = 500;
+let context;
+
+let playerWidth = 10;
+let playerHeight = 50;
+let playerVelocityY = 0;
+
+let player1 = {
+    x: 10,
+    y: (boardHeight / 2) - (playerHeight / 2),
+    width: playerWidth,
+    height: playerHeight,
+    velocityY: playerVelocityY
+};
+
+let player2 = {
+    x: boardWidth - playerWidth - 10,
+    y: (boardHeight / 2) - (playerHeight / 2),
+    width: playerWidth,
+    height: playerHeight,
+    velocityY: playerVelocityY
+};
+
+let ballWidth = 10;
+let ballHeight = 10;
+let ball = {
+    x: boardWidth / 2,
+    y: boardHeight / 2,
+    width: ballWidth,
+    height: ballHeight,
+    velocityY: 2,
+    velocityX: 1
+};
+
+
+let player1Score = 0;
+let player2Score = 0;
+
+
+window.onload = function() {
+    board = document.getElementById("board");
+    board.height = boardHeight;
+    board.width = boardWidth;
+    context = board.getContext("2d"); // used for drawing on the canvas/board
+
+    requestAnimationFrame(update);
+    document.addEventListener("keydown", movePlayer);
+    document.addEventListener("keyup", stopPlayer);
+}
+
+function update() {
+    context.clearRect(0, 0, board.width, board.height);
+    
+    // Update player positions
+    let nextPlayer1Y = player1.y + player1.velocityY;
+    if (!outOfBounds(nextPlayer1Y)) {
+        player1.y = nextPlayer1Y;
+    }
+
+    let nextPlayer2Y = player2.y + player2.velocityY;
+    if (!outOfBounds(nextPlayer2Y)) {
+        player2.y = nextPlayer2Y;
+    }
+
+    // Draw players
+    context.fillStyle = "skyblue";
+    context.fillRect(player1.x, player1.y, player1.width, player1.height);
+    context.fillRect(player2.x, player2.y, player2.width, player2.height);
+
+    // Update ball position
+    ball.x += ball.velocityX;
+    ball.y += ball.velocityY;
+    context.fillStyle = "white";
+    context.fillRect(ball.x, ball.y, ball.width, ball.height);
+
+    // Ball collision with top or bottom of canvas
+    if (ball.y <= 0 || (ball.y + ball.height >= boardHeight)) {
+        ball.velocityY *= -1; // reverse direction
+    }
+
+    // Ball collision with players
+    if (detectCollision(ball, player1)) {
+        if (ball.x <= player1.x + player1.width) {
+            // Left side of ball touches right side of Player1
+            ball.velocityX *= -1; // reverse x direction
+        }
+    } else if (detectCollision(ball, player2)) {
+        if (ball.x + ballWidth >= player2.x) {
+            // Right side of ball touches left side of Player2
+            ball.velocityX *= -1;
+        }
+    }
+
+
+    // game over
+    if (ball.x < 0) {
+        player2Score++
+        resetGame(1)
+    }
+    else if (ball.x + ballWidth > boardWidth) {
+        player1Score++
+        resetGame(-1)
+    }
+    // context.font = "1vw sans-serif"
+    context.fillText(player1Score, boardWidth / 5,45)
+    context.fillText(player2Score, boardWidth*4/5 -45,45)
+
+    for(let i = 10; i < board.height; i += 25) {
+        // i = starting Y position: Draw a square every 25 pixels
+        // x pos: half of board width -10, i = y pos. Square = 5 * 5 px
+        context.fillRect(board.width/2 - 10,i,5,5)
+    }
+    requestAnimationFrame(update);
+
+}
+
+function outOfBounds(yPosition) {
+    return yPosition < 0 || yPosition + playerHeight > boardHeight;
+}
+
+function movePlayer(e) {
+    console.log(e);
+    if (e.code == "KeyW") {
+        player1.velocityY = -3;
+    } else if (e.code == "KeyS") {
+        player1.velocityY = 3;
+    }
+
+    if (e.code == "ArrowUp") {
+        player2.velocityY = -3;
+    } else if (e.code == "ArrowDown") {
+        player2.velocityY = 3;
+    }
+}
+
+function stopPlayer(e) {
+    if (e.code == "KeyW" || e.code == "KeyS") {
+        player1.velocityY = 0;
+    }
+
+    if (e.code == "ArrowUp" || e.code == "ArrowDown") {
+        player2.velocityY = 0;
+    }
+}
+
+function detectCollision(a, b) {
+    return a.x < b.x + b.width &&    // a's top left corner doesn't reach b's top right corner
+           a.x + a.width > b.x &&    // a's top right corner passes b's top left corner
+           a.y < b.y + b.height &&   // a's top left corner doesn't reach b's bottom left corner
+           a.y + a.height > b.y;     // a's bottom left corner passes b's top left corner
+}
+
+function resetGame(direction) {
+    ball = {
+        x: boardWidth / 2,
+        y: boardHeight / 2,
+        width: ballWidth,
+        height: ballHeight,
+        velocityY: 2,
+        velocityX: direction
+    };
+    
+}
+
+// Let player first configure playfield width and height
+// Maybe also some styling.
+// Let the user save a configuration
+// Add a post-play screen.
+// Highscores
+// Multiplayer
